da3b5157e5e8eb5ad094677d1c5fee006175f267
merom <sonjoo1@dongyang.ac.kr>
2025-12-08 10:40:00 +0900

    초기 폴더 생성해야 오류잡힘 serverletContextListener 추가

---
+++ gimalProject/src/main/java/servletContextListener/UploadFolderListener.java
@@ -0,0 +1,28 @@
+package servletContextListener;
+
+import java.io.File;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+//이미지 저장 폴더를 외부로 빼서 다른 환경에서는 매번 폴더를 생성해야하는 번거로움 
+//-> 3.0이 지원하는 리스너 사용 서버 생성, 종료 시 실행되는 리스너 활용해서 폴더 만들기
+@WebListener
+public class UploadFolderListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        String baseUploadPath = "C:/upload";
+        File uploadDir = new File(baseUploadPath);
+        if (!uploadDir.exists()) {
+            boolean created = uploadDir.mkdirs();
+            System.out.println("업로드 기본 폴더 생성: " + created);
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        // 서버 종료 시 처리 필요 시 작성
+    }
+}
+
